fd960b5978f5746284c3b9625b1938aebe9c8387
sriaudel <sebastien.riaudel@laposte.net>
2025-06-06 16:27:12 +0200

    update

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,9 +9,8 @@ app.get('/mars', function(req, res) {
   res.send('Hello Mars!\n');
 });
 
-
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Exple app listening on port 8080!');
 });
 
 
